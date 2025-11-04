4bb3dfbac43c294a499af8af51053d81a9fd124b
Kazeous <vunguyen11204@gmail.com>
2025-11-04 11:23:34 +0700

    re-update since giscus decided to ragequit

--- src/config.ts
+++ src/config.ts
@@ -35,7 +35,7 @@ export const GiscusConfig: Record<string, string> = {
 	'data-reactions-enabled': '1',
 	'data-emit-metadata': '0',
 	'data-input-position': 'top',
-	'data-lang': 'en-US',
+	'data-lang': 'en',
 	'data-loading': 'lazy',
 	crossorigin: 'anonymous',
 	async: '',
